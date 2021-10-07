615a62206a3a78e2480238b6f44f99dae22ca0ce
MMartin <91570588+MartinMarcDWARF@users.noreply.github.com>
2021-10-07 18:33:20 +0200

    DT_sc start

---
+++ 00_journalists.rb
@@ -0,0 +1,114 @@
+def txt
+  puts "Bonjour, voici la liste des journalistes de Twitter.\n"
+  print "Appuyez sur 'Entrée' pour y accéder.\n"
+end
+
+def jlist
+  return j_tab = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+end
+
+def jlist_lgt(j_tab)
+  gets.chomp
+  puts "I- Combien y a-t-il de handle dans la liste ?\n\n"
+  for i in "Il y a exactement #{j_tab.length} allumettes dans cette boîte ;)".chars.to_a
+    print i
+    sleep 0.04
+  end
+end
+
+
+def sht_hnd(j_tab)
+  gets.chomp
+  puts "II- Quel est le nain de cette liste ?"
+      
+    
+      j_tab.sort_by!(&:length)
+      min_length = j_tab[0].length
+      result = j_tab[0]
+      j_tab.each do |element|
+        element.length < min_length ? (result = element) : result
+      end
+      for i in "Voici #{result} ! le digne représentant des peuplades nanesques !".chars.to_a
+        print i
+        sleep 0.04
+    end  
+  end
+
+
+def length_5(j_tab)
+  lgt5 = 0
+  gets.chomp
+  puts "III- Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)"
+  gets.chomp
+  j_tab.length.times do |index|
+    if j_tab[index].length == 6
+    lgt5 += 1
+    end  
+  end
+  for i in "ROUUUUUULEMENT DE TABOUREEEEEEEEEETS !!!\n\n\n\n. . . . . . . . . . . . . . .\n\n\n. . . . . . . . . . . . . . .\n\n\nAlors comme ça on attend ?\n\n\n\n".chars.to_a
+    print i
+    sleep 0.2
+  end
+  return lgt5
+  
+end
+
+
+
+
+def upcase_start(j_tab)
+  startuc = 0
+  gets.chomp
+  puts "IV- Combien y-a-t'il de handle commençant par une MAJ ?"
+  gets.chomp
+  j_tab.select {|v| v[1] == v[1].upcase}
+    if j_tab[v].select = v[1].upcase
+      startuc += 1
+      for i in "".chars.to_a
+      print i
+      sleep 0.2
+      end
+    end
+end
+
+
+
+
+
+def ord_lgt(j_tab)
+  gets.chomp
+  puts "VI- Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)\n\n"
+  for i in "Yes, my master...\n\n".chars.to_a
+    print i
+    sleep 0.2
+  end
+  puts j_tab.sort_by(&:length)
+  
+end
+
+
+def ord_alp(j_tab)
+  gets.chomp
+  puts ""
+  puts "V- Trie la liste de handle par ordre alphabétique.\n\n"
+  for i in "Dobby ne vit que pour servir le maître...".chars.to_a
+    print i
+    sleep 0.1
+  end
+  puts j_tab.sort_by(&:downcase)
+end
+
+
+
+def perform
+  txt
+  j_tab = jlist
+  #jlist_lgt(j_tab)
+  #sht_hnd(j_tab)
+  #puts length_5(j_tab)
+  puts upcase_start(j_tab)
+  ord_alp(j_tab)
+  ord_lgt(j_tab)
+end
+
+perform
